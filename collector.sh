d9ea741dc09d7ef91d0eb70cfbc2db0920e23943
rinamuka <58566654+rinamuka@users.noreply.github.com>
2021-07-05 23:39:36 +0300

    Update collector.sh

--- collector.sh
+++ collector.sh
@@ -1,1 +1,14 @@
+checkDependencies () {        ##### Check if aircrack-ng is installed or not #####
+if [ $(dpkg-query -W -f='${Status}' aircrack-ng 2>/dev/null | grep -c "ok installed") -eq 0 ]; then
+echo "Installing aircrack-ng\n\n"
+sudo apt-get install aircrack-ng;
+fi
+}
 
+checkWiFiStatus () {        ##### Check if wlan0 is enabled or not #####
+WiFiStatus=`nmcli radio wifi`
+if [ "$WiFiStatus" == "disabled" ]; then
+nmcli radio wifi on
+echo -e "[${Green}wlan0${White}] Enabled!"
+fi
+}
