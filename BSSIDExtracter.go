5b99694435e4d00721a3f520a876f16b7aed3c03
rinamuka <58566654+rinamuka@users.noreply.github.com>
2021-07-09 21:24:01 +0300

    Create BSSIDExtracter.go

---
+++ BSSIDExtracter.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"encoding/csv"
+	"io"
+	"log"
+	"os"
+)
+
+func main() {
+	file, err := os.Open(os.Args[1])
+	if err != nil {
+		panic(err)
+	}
+	defer file.Close()
+	reader := csv.NewReader(file)
+	reader.FieldsPerRecord = 15
+	reader.Comment = '#'
+	BSSIDs := []string{}
+	for i := 0; i <= 10; i++ {
+
+		record, err := reader.Read()
+		if err != nil {
+			log.Println(err)
+			break
+		}
+		if i == 0 {
+			continue
+		}
+		BSSIDs = append(BSSIDs, record[0])
+	}
+
+	file, err = os.OpenFile("onlyBSSID",os.O_CREATE|os.O_RDWR, 0777)
+	if err!=nil{
+		log.Fatal(err)
+	}
+	for _, BSSID:=range BSSIDs{
+		io.WriteString(file, BSSID + "\n")
+	}
+	file.Close()
+
+
+}
